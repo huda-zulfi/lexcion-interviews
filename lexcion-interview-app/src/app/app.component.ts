9a999544066fec1ec62ed14cc7cf0537db1d5bda
asim <you@example.com>
2021-12-24 12:21:49 -0600

    added welcome component

--- lexcion-interview-app/src/app/app.component.ts
+++ lexcion-interview-app/src/app/app.component.ts
@@ -5,6 +5,8 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
+  
 export class AppComponent {
+  path = "welcome";
   title = 'lexcion-interview-app';
 }
