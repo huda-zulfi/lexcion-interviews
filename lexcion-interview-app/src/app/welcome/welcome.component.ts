e8502f7ab2ecee5ccd2a8c54855b7081d4d4ab74
asim <you@example.com>
2021-12-24 12:20:12 -0600

    created new component

---
+++ lexcion-interview-app/src/app/welcome/welcome.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lexi-welcome',
+  templateUrl: './welcome.component.html',
+  styleUrls: ['./welcome.component.less'],
+})
+export class WelcomeComponent implements OnInit {
+  data: any = [
+    {
+      cosultantCompany: 'OlympiaCG',
+      endClient: 'citi',
+      jobTitle: 'Sr. Developer',
+      jobTech: ['node', 'angular'],
+      levelInterview: 'two',
+      date: new Date(),
+      recuriterName: 'asd',
+      recuriterEmail: 'asdfdf',
+    },
+    {
+      cosultantCompany: 'Zulifqar',
+      endClient: 'citi',
+      jobTitle: 'Sr. Developer',
+      jobTech: ['node', 'angular'],
+      levelInterview: 'two',
+      date: new Date(),
+      recuriterName: 'asd',
+      recuriterEmail: 'asdfdf',
+    },
+    {
+      cosultantCompany: 'Malik',
+      endClient: 'citi',
+      jobTitle: 'Sr. Developer',
+      jobTech: ['node', 'angular'],
+      levelInterview: 'two',
+      date: new Date(),
+      recuriterName: 'asd',
+      recuriterEmail: 'asdfdf',
+    },
+  ];
+  constructor() {}
+
+  ngOnInit(): void {}
+}
