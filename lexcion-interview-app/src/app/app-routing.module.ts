a8c85b07f18d4560637e3ceaab1c847d92bf6b0d
asim <you@example.com>
2022-01-03 22:12:44 -0600

    added edit interview

--- lexcion-interview-app/src/app/app-routing.module.ts
+++ lexcion-interview-app/src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DetailsComponent } from './details/details.component';
+import { EditInterviewComponent } from './edit-interview/edit-interview.component';
 import { PostDetailComponent } from './post-detail/post-detail.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 
 const routes: Routes = [
   { path: 'welcome', component: WelcomeComponent },
   { path: 'details/:id/', component: DetailsComponent },
-  { path: 'create-interview', component:PostDetailComponent },
+  { path: 'create-interview', component: PostDetailComponent },
+  { path: 'edit-interview/:id/', component: EditInterviewComponent },
 ];
 
 @NgModule({
--- lexcion-interview-app/src/app/edit-interview/edit-interview.component.ts
+++ lexcion-interview-app/src/app/edit-interview/edit-interview.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { InterviewDataService } from '../interview-data.service';
 
 @Component({
   selector: 'lexi-edit-interview',
   templateUrl: './edit-interview.component.html',
-  styleUrls: ['./edit-interview.component.less']
+  styleUrls: ['./edit-interview.component.less'],
 })
 export class EditInterviewComponent implements OnInit {
+  constructor(private interviewData: InterviewDataService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
